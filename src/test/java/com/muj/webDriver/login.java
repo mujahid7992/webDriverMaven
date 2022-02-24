ed0ece1bd43d3f8ffcf9cb413cd6fbafcfb32359
mujahid7992 <sandmujju@gmail.com>
2022-02-24 20:05:11 +0530

    Initial commit

---
+++ src/test/java/com/muj/webDriver/login.java
@@ -0,0 +1,28 @@
+package com.muj.webDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class login {
+public static WebDriver driver;
+@BeforeSuite
+public void setUp() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shaik Mujahed\\Desktop\\chromedriver98\\chromedriver\\chromedriver.exe");
+	
+	driver= new ChromeDriver();
+	
+}
+@Test
+public void google() {
+	driver.get("http://google.com");
+	
+}
+@AfterSuite
+public void tearDown() {
+	driver.quit();
+	
+}
+}
